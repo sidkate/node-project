d9fcd6ded62be30a9b0f0aff8bcaf23f22f0ad32
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-12 21:21:12 +0400

    delete from catalog

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { listCatalog } = require('./list.js');
 const { addToCatalog } = require('./add.js');
+const { deleteFromCatalog } = require('./delete.js');
 const { menu } = require('./menu.js');
 
 const menuItems = {
     1: { text: "list catalog", func: (cb) => { listCatalog(cb) } },
     2: { text: "add product to catalog", func: (cb) => { addToCatalog(cb) } },
-    3: { text: "delete product from catalog", func: (cb) => { cb()/* some logic */ } },
+    3: { text: "delete product from catalog", func: (cb) => { deleteFromCatalog(cb) } },
     4: { text: "change product in catalog", func: (cb) => { cb()/* some logic */ } },
     0: { text: "exit", func: (cb) => { process.exit(0) } },
 }
