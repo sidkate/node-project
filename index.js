c2dc14d26adbb52227f2cd442bedcff85aeeec90
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-12 21:53:18 +0400

    change catalog item

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { listCatalog } = require('./list.js');
 const { addToCatalog } = require('./add.js');
 const { deleteFromCatalog } = require('./delete.js');
+const { changeInCatalog } = require('./change.js');
 const { menu } = require('./menu.js');
 
 const menuItems = {
     1: { text: "list catalog", func: (cb) => { listCatalog(cb) } },
     2: { text: "add product to catalog", func: (cb) => { addToCatalog(cb) } },
     3: { text: "delete product from catalog", func: (cb) => { deleteFromCatalog(cb) } },
-    4: { text: "change product in catalog", func: (cb) => { cb()/* some logic */ } },
+    4: { text: "change product in catalog", func: (cb) => { changeInCatalog(cb) } },
     0: { text: "exit", func: (cb) => { process.exit(0) } },
 }
 
