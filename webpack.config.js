894ecf99d5fba9d1fed7178b2dd936b500ab0eb5
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-21 22:55:53 +0400

    building frontend using webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        port: 3000
+    },
+    entry: path.resolve(__dirname, 'src/frontend/scripts/ajax.js'),
+    output: {
+        path: path.resolve(__dirname, 'src/public'),
+        filename: 'main.js',
+        library: 'myAjax' // access to exported functions from web
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            /*{
+                test: /\.(png|jpg|jpeg|svg)/,
+                use: 'file-loader'
+            },*/
+            {
+                test: /\.css$/,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ]
+
+}
