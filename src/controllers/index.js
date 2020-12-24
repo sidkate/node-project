b99bd2b53348210b21599420e07a5e3d45cdb91b
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-24 21:28:21 +0400

    moved admin-related code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const homeController = require('./home.controller');
-//const usersController = require('./users.controller');
-const productsController = require('./products.controller');
+const adminHomeController = require('./admin/home.controller');
+const adminProductsController = require('./admin/products.controller');
 const apiControllers = require('./api');
 
 const router = new Router();
 
-router.use('/', homeController);
-//router.use('/users', usersController);
-router.use('/products', productsController);
+router.use('/admin/', adminHomeController);
+router.use('/admin/products', adminProductsController);
 router.use('/api', apiControllers);
-
+router.use('/', homeController);
 module.exports = router;
