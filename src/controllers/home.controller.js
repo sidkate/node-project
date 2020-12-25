b42ce508fd3459e98fd6bae835d38d6b93d8151b
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-25 19:42:12 +0400

    products initial logic

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,9 +1,16 @@
+const productsRepository = require('../repositories/products.repository');
+
 const { Router } = require('express');
 
 const router = new Router();
 
 router.get('/', (_request, response) => {
     response.render('pages/home');
+});
+
+router.get('/products', async (_request, response) => {
+    const products = await productsRepository.getAll();
+    response.render('pages/products/view', { products });
 });
 
 module.exports = router;
