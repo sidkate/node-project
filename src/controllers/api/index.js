f0fffb0ae7274af97653fb5565cf75bcacf683f4
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-27 15:13:58 +0400

    obsolete code

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,10 +1,7 @@
 const { Router } = require('express');
-//const usersController = require('./users.controller');
 const productsController = require('./products.controller');
 
 const router = new Router();
-
-//router.use('/users', usersController);
 
 router.use('/products', productsController);
 
